267b768a9339059558f37d74d7b354f1a0d32292
Tetsuya Wada <tetsukamen00@gmail.com>
2021-10-26 18:37:19 +0900

    データベースの作成

---
+++ models/genre.js
@@ -0,0 +1,14 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var Genre = new Schema({
+  name: { type: String, required: true, max: 100, min: 3 },
+});
+
+Genre.virtual("url").get(function () {
+  return "/catalog/genre/" + this._id;
+});
+
+//Export model
+module.exports = mongoose.model("Genre", Genre);
